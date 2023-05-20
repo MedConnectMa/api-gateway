124e850459f34a3ddabcea901429c1a24ea4c2c6
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-05-20 03:00:22 +0100

    feature: self-register services

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
 
-// app
+//** app **//
 const app = express();
 const port = 8000;
 
-// proxies
-const userProxy = httpProxy.createProxyServer({
-  target: 'http://user_service'
+//** services **//
+const services = [];
+
+// register
+app.post('/register/:service', (req, res) => {
+    const service = req.params.service;
+    if (services[service])
+        res.status(400).send(`Service ${service} already registered`);
+    else {
+        services[service] = httpProxy.createProxyServer({
+            target: `http://${service}-service`
+        });
+        res.status(200).send(`Service ${service} registered`);
+    }
 });
 
-// routes
-app.all('/api/users/*', (req, res) => {
-  userProxy.web(req, res);
+// proxy
+app.all('/api/:service/*', (req, res) => {
+    const service = req.params.service;
+    if (services[service])
+        services[service].web(req, res);
+    else
+        res.status(400).send(`Service ${service} does not exist`);
 });
 
-// start server
+// not found
+app.all('*', (req, res) => {
+    res.status(404).send('Not Found');
+});
+
+//** start **//
 app.listen(port, () => {
   console.log(`Gateway listening at http://0.0.0.0:8000`);
 });
